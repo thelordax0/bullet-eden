d23656862c528528c7bd3f84c5855226b6e36276
thelordax0 <thelordaxe05@gmail.com>
2024-01-28 21:49:46 +0300

    add Game thread

--- src/main/java/org/thelordax0/bulleteden/Game.java
+++ src/main/java/org/thelordax0/bulleteden/Game.java
@@ -1,7 +1,28 @@
 package org.thelordax0.bulleteden;
 
-public class Game {
+public class Game implements Runnable{
     public static int width=300;
     public static  int height=width/16*9;
     public static int scale=3;
+
+    private Thread thread;
+
+    public synchronized void start(){
+        thread=new Thread(this,"Display");
+        thread.start();
+    }
+    public synchronized void stop(){
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+    @Override
+    public void run() {
+
+    }
 }
